5464fdc43c5283609c3954a6619c8c0b116868f5
Brian Capouch <brianc@saintjoe.edu>
2014-12-17 03:08:03 -0500

    More collaboration testing

--- FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
+++ FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
@@ -3,7 +3,7 @@ package edu.saintjoe.cs.brianc.git.first;
 public class TestClass {
 	public static void main(String[] args ) {
 		System.out.println("Hello there, git world!");
-		System.out.println("I have modified an existing file!");
+		System.out.println("I have modified an existing file, from two places!");
 	}
 
 }
