a32af664f9c5b19ac68219c3cad6ff0aef285288
Brian Capouch <brianc@saintjoe.edu>
2014-12-17 07:57:38 +0000

    Test collaboration

--- FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
+++ FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
@@ -3,7 +3,7 @@ package edu.saintjoe.cs.brianc.git.first;
 public class TestClass {
 	public static void main(String[] args ) {
 		System.out.println("Hello there, git world!");
-		System.out.println("I have added something new!");
+		System.out.println("I have modified an existing file!");
 	}
 
 }
