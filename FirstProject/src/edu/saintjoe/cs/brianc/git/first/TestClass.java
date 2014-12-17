66495d708fa15ec49b060d39e0374df39229e19e
Brian Capouch <brianc@saintjoe.edu>
2014-12-17 02:53:22 -0500

    Add new print statement

--- FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
+++ FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
@@ -3,6 +3,7 @@ package edu.saintjoe.cs.brianc.git.first;
 public class TestClass {
 	public static void main(String[] args ) {
 		System.out.println("Hello there, git world!");
+		System.out.println("I have added something new!");
 	}
 
 }
