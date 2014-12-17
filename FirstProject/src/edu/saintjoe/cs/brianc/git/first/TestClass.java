6ffde90e57a3de1851f0d36019795a7578f85fcb
Brian Capouch <brianc@saintjoe.edu>
2014-12-17 02:04:42 -0500

    Changed main print message

--- FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
+++ FirstProject/src/edu/saintjoe/cs/brianc/git/first/TestClass.java
@@ -2,7 +2,7 @@ package edu.saintjoe.cs.brianc.git.first;
 
 public class TestClass {
 	public static void main(String[] args ) {
-		System.out.println("Hello world!");
+		System.out.println("Hello there, git world!");
 	}
 
 }
